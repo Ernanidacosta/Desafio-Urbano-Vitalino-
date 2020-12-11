b0e42fa4ce1f3bcd5be0e21fea8884ce675459ba
Ernani N. da Costa <ernanidacosta@gmail.com>
2020-12-11 18:19:00 +0000

    implements poetry

--- desafio/desafio/settings.py
+++ desafio/desafio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'processos',
+    'cadastro_processo',
 ]
 
 MIDDLEWARE = [
